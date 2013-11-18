93dce1da7bfdf521aa257a0562703e348d5e0ee4
Rob Spieldenner <rob.spieldenner@gmail.com>
2013-11-18 07:56:27 -0800

    add dotfiles

---
+++ dotfiles/link-dotfiles.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+ln -s gitconfig ~/.gitconfig
+ln -s bashrc ~/.bashrc
+ln -s bash_profile ~/.bash_profile
+ln -s zshrc ~/.zshrc
+ln -s zprofile ~/.zprofile
+ln -s zshenv ~/.zshenv
+ln -s aliases ~/.aliases
